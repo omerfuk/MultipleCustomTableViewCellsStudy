03e570cd5a88bb97c96beaf6b20cf37a7dad3283
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-12 12:16:48 +0300

    Initial Commit

---
+++ MultipleCustomTableViewCellsStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleCustomTableViewCellsStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
