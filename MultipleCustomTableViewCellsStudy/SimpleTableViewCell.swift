92db5ef7a3773b854a39e860853fcf64c5ac006f
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-12 12:27:47 +0300

    12.00 academy,

---
+++ MultipleCustomTableViewCellsStudy/SimpleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SimpleTableViewCell.swift
+//  MultipleCustomTableViewCellsStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 12.07.2022.
+//
+
+import UIKit
+
+class SimpleTableViewCell: UITableViewCell {
+
+    static let identifier = "SimpleTableViewCell"
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
--- MultipleCustomTableViewCellsStudy/ViewController.swift
+++ MultipleCustomTableViewCellsStudy/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private let tableView: UITableView = {
+        
+        let table = UITableView()
+        
+        table.register(SimpleTableViewCell.self, forCellReuseIdentifier: SimpleTableViewCell.identifier)
+        
+        table.register(ImageTableViewCell.nib(), forCellReuseIdentifier: ImageTableViewCell.identifier)
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row < 5 {
+            //Show image cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.identifier, for: indexPath) as! ImageTableViewCell
+            cell.configure(with: "image1")
+            return cell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: SimpleTableViewCell.identifier, for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+        
+    }
+    
 }
 
